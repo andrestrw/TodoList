795148967599f3db0637a8c1e63ae1bbfb402283
Andrés <jandres33110@gmail.com>
2025-04-15 07:05:02 +0200

    add components for my app

---
+++ mytodolist/src/app/components/TaskDescription.tsx
@@ -0,0 +1,12 @@
+
+
+export default function TaskDescription(props: any) {
+    //console.log(props)
+    return (
+        <>
+          
+                <div className="border-2 border-solid py-2 px-2">{props.task}</div>
+            
+        </>
+    )
+}
---
+++ mytodolist/src/app/components/TitleDescription.tsx
@@ -0,0 +1,13 @@
+
+
+export default function TaskTitle(props: any) {
+    //console.log(props)
+    return (
+
+        <>
+      
+            <div className="border-2 border-solid py-2 px-2">{props.task}</div>
+       
+        </>
+    )
+}
